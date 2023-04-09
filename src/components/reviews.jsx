ba86bbc12b54a589b5c9cc90ab49f9aceb3a6106
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-04-09 21:29:19 +0200

    rev

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -1,21 +1,20 @@
 import { useParams } from 'react-router-dom';
 import { fetchMovieReviews } from './fetchAPI';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 const Reviews = () => {
   const [movieRev, setRev] = useState([]);
   const { id } = useParams();
 
-  const fetchRev = useCallback(async () => {
-    const details = await fetchMovieReviews(id);
-
-    setRev(details);
-  }, [id]);
-
   useEffect(() => {
+    const fetchRev = async () => {
+      const details = await fetchMovieReviews(id);
+
+      setRev(details);
+    };
     fetchRev();
-  }, [fetchRev]);
+  }, [id]);
 
   const { results, total_results } = movieRev;
   return (
